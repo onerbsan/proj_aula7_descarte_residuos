6645cc64fdbf0947fe5b6f589143726a0083daa2
Onerb <109546803+onerbsan@users.noreply.github.com>
2025-11-13 22:36:55 -0300

    Add MongooseModule for MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DescarteModule } from './descarte/descarte.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [DescarteModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://onerB_san:onerB.san03@onerbs.zgeuulu.mongodb.net/proj_aula7?appName=onerBs'),
+    DescarteModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
